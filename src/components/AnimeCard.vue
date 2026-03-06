9c9cba051d5c05dc8f6d6c5b0232f2d3fe88ed1d
lperaza2 <lperaza2@stu.southtexascollege.edu>
2026-03-05 21:26:51 -0600

    start template animecard

---
+++ src/components/AnimeCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <section
+    class="rounded-3xl border border-slate-700/70 bg-slate-900/60 p-5 shadow-2xl shadow-slate-950/30 backdrop-blur-sm"
+  >
+    <div></div>
+  </section>
+</template>
