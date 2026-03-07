0b797163a57c46e6e950f1a75273d8f74fa14f04
lperaza2 <lperaza2@stu.southtexascollege.edu>
2026-03-06 19:05:01 -0600

    playing with layout in spin

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,14 @@
 <script setup>
+import { ref } from 'vue'
 import AnimeCard from '@/components/AnimeCard.vue'
 
-const spin = () => console.log('Hello')
+const loading = ref(false)
+const error = ref('')
+
+const spin = () => {
+  if (error.value.length > 0) error.value = ''
+  else error.value = 'Something wrong'
+}
 </script>
 
 <template>
@@ -39,7 +46,10 @@ const spin = () => console.log('Hello')
               </button>
             </div>
           </div>
-          <AnimeCard loading />
+          <AnimeCard
+            :loading="loading"
+            :error="error"
+          />
         </section>
       </div>
     </div>
